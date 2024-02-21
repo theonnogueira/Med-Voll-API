f6faa5deb5c5cb20a0ac496e36ebe5f1014ee7c8
Théo Nogueira <theonnogueira@outlook.com>
2024-02-21 17:31:43 -0300

    Adicionando variável telefone na entidade médico

--- api/src/main/java/med/voll/api/medico/DadosCadastroMedico.java
+++ api/src/main/java/med/voll/api/medico/DadosCadastroMedico.java
@@ -16,6 +16,8 @@ public record DadosCadastroMedico(
         @NotBlank
         @Pattern(regexp = "\\d{4,6}")
         String crm,
+        @NotBlank
+        String telefone,
         @NotNull
         Especialidade especialidade,
         @NotNull
