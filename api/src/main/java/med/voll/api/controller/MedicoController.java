5d092bb44db1044ce46d90b65b9c6b5e62427dd4
Théo Nogueira <theonnogueira@outlook.com>
2024-02-21 12:06:16 -0300

    Médico Controller - Record - ENUM

---
+++ api/src/main/java/med/voll/api/controller/MedicoController.java
@@ -0,0 +1,15 @@
+package med.voll.api.controller;
+
+import med.voll.api.medico.DadosCadastroMedico;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/medicos")
+public class MedicoController {
+
+    @PostMapping
+    public void register(@RequestBody DadosCadastroMedico dados) {
+        System.out.println(dados);
+    }
+
+}
